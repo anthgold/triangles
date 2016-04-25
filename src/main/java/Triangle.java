ab8d7f6dd4644dda9a71c1e5aa142734d617a5cc
Anthony Gold and Elissa Sosland <anthony@>
2016-04-25 16:17:27 -0700

    adds passing equilateral test

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,4 +1,21 @@
 public class Triangle {
+  private int mSideX;
+  private int mSideY;
+  private int mSideZ;
+
+  public Triangle (int sideX, int sideY, int sideZ) {
+    mSideX = sideX;
+    mSideY = sideY;
+    mSideZ = sideZ;
+  }
+
+  public String getTriangleType() {
+    if (mSideX == mSideY && mSideY == mSideZ) {
+      return "equilateral";
+    } else {
+      return "";
+    }
+  }
 
 /*  private int mLength;
   private int mWidth;
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -6,6 +6,12 @@ public class TriangleTest {
   @Test
   public void newTriangle_instantiatesCorrectly() {
     Triangle testTriangle = new Triangle(2,2,4);
-    assertEquals(true, testTriangle instanceof Triangle); // should fail
+    assertEquals(true, testTriangle instanceof Triangle);
+  }
+
+  @Test
+  public void newTriangle_isEquilateralTriangle_equilateral() {
+    Triangle testTriangle = new Triangle(1,1,1);
+    assertEquals("equilateral", testTriangle.getTriangleType());
   }
 }
